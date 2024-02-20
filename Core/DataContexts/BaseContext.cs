f1ed7f5d8b3d42706a91b993ba325bf2e0ac1f4e
sim1-dev <simonetenisci10@gmail.com>
2024-02-20 15:21:33 +0100

    [wip] Seeder: Projects

--- Core/DataContexts/BaseContext.cs
+++ Core/DataContexts/BaseContext.cs
@@ -96,13 +96,48 @@ public class BaseContext: IdentityDbContext {
     {
         modelBuilder.Entity<Project>().HasData(
 
+            // IdeaIT
             new Project { Id = 1, Name = "Realizzazione Sito ShareLock", Description = """Realizzazione e implementazione tema custom compatibile con il software Elementor, manutenzione generale sui contenuti e sulla sicurezza""", Start = DateTime.Parse("2019-04-03"), End = DateTime.Parse("2019-07-14"), CompanyId = 1 },
 
+            // GSE
             new Project { Id = 2, Name = "Realizzazione Gestionale SuiteCRM", Description = """Ricostruzione dell'intero ambiente da zero con moduli custom, implementazione di funzionalità di WorkFlow, configurazione per il corretto utilizzo delle cartelle di posta essoteriche Exchange e riadattamenti vari per consentire la corretta comunicazione con il Database SQL Server""", Start = DateTime.Parse("2021-06-03"), End = DateTime.Parse("2021-12-14"), CompanyId = 2 },
 
+            // BIP
             new Project { Id = 3, Name = "Realizzazione Applicativo Tableau De Bord", Description = """Portale con frontend in Vue.js che offre ai suoi utenti dei grafici sull'andamento delle attività e turni lavorativi""", Start = DateTime.Parse("2020-06-16"), End = DateTime.Parse("2021-01-17"), CompanyId = 2 },
 
-            new Project { Id = 4, Name = "Realizzazione Applicativo Bacheca", Description = """Portale con frontend in Vue.js che offre ai suoi utenti un'interfaccia per consultare le novità e un classico CRUD amministrativo per la gestione dei contenuti""", Start = DateTime.Parse("2020-08-04"), End = DateTime.Parse("2020-10-11"), CompanyId = 2 }
+            new Project { Id = 4, Name = "Realizzazione Applicativo Bacheca", Description = """Portale con frontend in Vue.js che offre ai suoi utenti un'interfaccia per consultare le novità e un classico CRUD amministrativo per la gestione dei contenuti""", Start = DateTime.Parse("2020-08-04"), End = DateTime.Parse("2020-10-11"), CompanyId = 2 },
+
+            // Sogeit
+            new Project { Id = 5, Name = "Revisione Applicativo Netbook", Description = """Implementazione funzionalità di generazione ed esportazione di carte topografiche, manutenzione generale""", Start = DateTime.Parse("2020-09-02"), End = DateTime.Parse("2021-03-17"), CompanyId = 2 },
+
+            new Project { Id = 6, Name = "Revisione Applicativo NewsletterPro", Description = """Potenziamento e ottimizzazione delle funzionalità di scheduling email""", Start = DateTime.Parse("2020-09-18"), End = DateTime.Parse("2020-12-19"), CompanyId = 2 },
+
+            new Project { Id = 7, Name = "Revisione Applicativo SmallApp", Description = """Creazione scheduling invio SMS in diverse lingue, implementazione funzionalità per l'automatizzazione del flusso, manutenzione generale sulla sicurezza""", Start = DateTime.Parse("2020-08-08"), End = DateTime.Parse("2021-06-09"), CompanyId = 2 },
+
+            new Project { Id = 8, Name = "Revisione Applicativo ASM", Description = """Aggiunta di funzionalità e bugfix vari""", Start = DateTime.Parse("2020-09-25"), End = DateTime.Parse("2021-02-01"), CompanyId = 2 },
+
+            new Project { Id = 9, Name = "Revisione Sito Operazione Risorgimento Digitale", Description = """Manutenzione generale, aggiunta di contenuti, creazione di un modulo custom""", Start = DateTime.Parse("2021-03-22"), End = DateTime.Parse("2021-10-21"), CompanyId = 2 },
+
+            new Project { Id = 10, Name = "Realizzazione Sito TIM Ventures", Description = """Creazione di un sito multilingua con tema custom, modulo custom e blocchi custom editabili da pannello di amministrazione""", Start = DateTime.Parse("2021-04-04"), End = DateTime.Parse("2021-04-25"), CompanyId = 2 },
+
+            new Project { Id = 11, Name = "Rifacimento Sito TIM Operazione Risorgimento Digitale", Description = """Creazione e manutenzione di un sito con tipi di contenuto filtrabili, editabili e gestibili dall'utente amministratore tramite blocchi di paragrafi e viste custom, logica di sottoscrizione a corsi, creazione di moduli custom""", Start = DateTime.Parse("2021-10-14"), End = DateTime.Parse("2022-04-12"), CompanyId = 2 },
+
+            // Foodlovery
+            new Project { Id = 12, Name = "Food Delivery", Description = """Implementazione sistema di notifiche E-Mail e OneSignal, con pannello gestionale lato web-app in base al tipo notifica e ruolo d'appartenenza, e piattaforma di messaggistica tramite ChatBot full-stack (Ionic per le tre app mobile, Angular per la web-app e NestJS per il Backend) per la risoluzione degli ordini non completati. Risoluzione di criticità varie presenti sulle quattro app frontend e riadattamento del sistema di geolocalizzazione tramite le API di Google Maps""", Start = DateTime.Parse("2022-02-06"), End = DateTime.Parse("2022-04-18"), CompanyId = 2 },
+
+            // Deep Consulting
+            new Project { Id = 13, Name = "Console Regia", Description = """Ricostruzione in Angular 11 di un applicativo Flash finalizzato alla gestione smart dei disservizi e implementazione di nuove funzionalità custom volte al filtraggio avanzato, alla visualizzazione di grafici statistici e all'esportazione dei dati intabellati in formato XLS. Prima bozza dell'applicativo costruita mediante l'utilizzo della libreria NGBootstrap, versione corrente mediante PrimeNG""", Start = DateTime.Parse("2021-10-27"), End = DateTime.Parse("2022-02-03"), CompanyId = 2 },
+
+            new Project { Id = 14, Name = "Intranet", Description = """Progettazione e sviluppo, nel contesto del Sistema informativo Aziendale interno, di una piattaforma software per la gestione del recruiting, delle richieste di lavoro, CV e degli skill matrix aziendali, con sistema di notifiche basato su Websockets, integrazione con le API di Google per lo scheduling dei colloqui da remoto e con Google Drive per il salvataggio di documenti importanti sul Cloud. Il backend è stato scritto in ambiente Linux / Apache e consta di un set di API RESTful sviluppate con l'ausilio di Laravel; la versione attuale del frontend in Angular 12 sviluppata con l'ausilio di Bulma e la libreria di componenti DevExtreme, la prima versione in Vue.js/Bootstrap 4. La Web App è stata realizzata con l'ausilio di Capacitor per il rilascio delle versioni Desktop Electron, Android e iOS.""", Start = DateTime.Parse("2020-12-03"), End = DateTime.Parse("2023-01-17"), CompanyId = 2 },
+
+            // Livebox
+            new Project { Id = 15, Name = "VDesk - Taranto", Description = """Realizzazione di un plugin full-stack “scadenziario” per l'app Angular 9 VDesk (Material - Syncfusion), con Backend Nextcloud 17 (PHP 7.3) e Bridge Laravel 6.2""", Start = DateTime.Parse("2022-03-18"), End = DateTime.Parse("2022-04-11"), CompanyId = 2 },
+
+            new Project { Id = 16, Name = "VDesk - Bologna", Description = """Realizzazione di un plugin full-stack per l'interfacciamento con un secondo DB esterno, con CRUD di tabelle/campi/relazioni e un'interfaccia frontend simil-phpMyAdmin. Realizzazione di un sistema di auto-provisioning degli utenti SAML importati dal cliente su tabelle di appoggio presenti un secondo DB esterno. Realizzazione di un plugin per la visualizzazione tramite menù a tendina condizionali delle informazioni presenti sull'area di staging all'interno del WorkFlow Builder, al fine di garantirne la fruibilità lato utente tramite campi compilabili dinamici. Implementazione di logica sottesa all'area di staging, finalizzata alla categorizzazione dinamica dei dati recuperati dai menù a tendina a seconda del gruppo di appartenenza dell'utente, fornendo all'editor del template la possibilità di specificare da backoffice una condizione di recupero dati per ogni gruppo esistente. Revisione della struttura logica del WorkFlow Builder volta all'implementazione e alla gestione di condizionalità di flusso, fornendo all'utente richiedente la possibilità di scegliere i gruppi destinatari della sua pratica su ogni RuoloFase specificato in backoffice tra una lista autopopolata di gruppi dipendenti dal proprio responsabile approvativo SAML. Implementazione di una casistica “flusso semplificato” per la gestione di richieste indipendenti dall'andamento del WorkFlow, che vengono convertite in PDF e inviate a uno o più indirizzi di posta elettronica come indicato in backoffice. Risoluzione di alcune criticità sulla parte di autenticazione SAML, display feed RSS, validazione elementi richiesta, personalizzazione email/messaggi notifiche e implementazione di funzionalità di ordinamento dati importati tramite CSV sui menù a tendina""", Start = DateTime.Parse("2022-04-12"), End = DateTime.Parse("2022-10-02"), CompanyId = 2 },
+
+            // TeamSystem
+            new Project { Id = 17, Name = "Presenze/Polyedro/AllInOne", Description = """Manutenzione generale, porting e nuove implementazioni su un macroapplicativo consolidato per il censimento dei dati anagrafici e verifiche sullo stato di attività della popolazione multiaziendale, gestione trasferte e registrazione contabile note spesa, elaborazione timbrature di presenza e produzione valorizzate da cartellino, pianificazione orari di lavoro, turni e allocazioni su reparti con ribaltamento diretto dei dati approvati su timesheet. Sviluppo di un applicativo in C per il recupero delle informazioni riguardanti sistema operativo, macchina e installazione sugli impianti on-premise. Ripensamento della logica di funzionamento e riscrittura del vecchio workflow autorizzativo delle giustificazioni via mail da C in C++, per consentire l'autorizzazione massiva automatica di un numero elevato di giustificativi in tempo reale. Sviluppo di servizi C++ per l'esposizione di dati su visibilità gerarchica del proprio team di lavoro verso App Mobile, sistemi SAP e dashboard esterne gestite dai clienti. Previsione di uno script per l'esportazione e il seeding di un preset di dati per agevolare l'installazione dei nuovi tenant cloud. Supporto all'analisi di un sistema di import parametrico preesistente in C++ per la messa in piedi della sua controparte di export in Python. Parte legacy sviluppata in C/Javascript, moderna con backend C++, Bridge Java 8 e frontend Angular 14.""", Start = DateTime.Parse("2022-09-14"), End = DateTime.Parse("2024-01-14"), CompanyId = 2 }
+
         );
     }
 
@@ -126,7 +161,8 @@ public class BaseContext: IdentityDbContext {
             new User { UserName = "user1", FirstName = "Emma", LastName = "Stone", Email = "emma.stone@mail.com" },
             new User { UserName = "user2", FirstName = "Liam", LastName = "Smith", Email = "liam.smith@mail.com" },
             new User { UserName = "user3", FirstName = "Olivia", LastName = "Jones", Email = "olivia.jones@mail.com" },
-            new User { UserName = "user4", FirstName = "Noah", LastName = "Brown", Email = "noah.brown@mail.com" }
+            new User { UserName = "user4", FirstName = "Noah", LastName = "Brown", Email = "noah.brown@mail.com" },
+            new User { UserName = "simone.tenisci", FirstName = "Simone", LastName = "Tenisci", Email = "simone.tenisci@r2u.it" }
         );
     }
 
